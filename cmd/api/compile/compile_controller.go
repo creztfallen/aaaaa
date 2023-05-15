af1aa41fc78358c51d22e8c03b43af22830ee36e
creztfallen <mtssmoreno@gmail.com>
2023-05-15 10:32:48 -0300

    refactor: add s3 function in utils package

--- cmd/api/compile/compile_controller.go
+++ cmd/api/compile/compile_controller.go
@@ -1,22 +1,13 @@
 package compile
 
 import (
-	"bytes"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/creztfallen/compiler_api/cmd/api/config"
 	"github.com/creztfallen/compiler_api/cmd/api/responses"
 	"github.com/creztfallen/compiler_api/cmd/api/utils"
 	"github.com/gofiber/fiber/v2"
-	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"sync"
 	"time"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 var (
@@ -51,42 +42,9 @@ func Compile(c *fiber.Ctx) error {
 			Data:    nil})
 	}
 
-	sess, err := session.NewSession(&aws.Config{
-		Region:           aws.String("sa-east-1"),
-		S3ForcePathStyle: aws.Bool(true),
-		Credentials:      credentials.NewStaticCredentials(config.AWS_KEY(), config.AWS_SECRET_KEY(), ""),
-	})
-	if err != nil {
-		return err
-	}
+	_ = utils.S3Upload(c, content, counter)
 
-	s3Client := s3.New(sess)
-
-	uploadInput := &s3.PutObjectInput{
-		Bucket: aws.String(config.AWS_BUCKET()),
-		Key:    aws.String(strconv.Itoa(counter) + "-P24_HGUV6.5.3B01_UPGRADE.bin"),
-		Body:   bytes.NewReader(content),
-	}
-
-	_, err = s3Client.PutObject(uploadInput)
-
-	if err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(responses.DefaultResponse{
-			Status:  http.StatusInternalServerError,
-			Message: "Couldn't send the file to bucket",
-			Data:    &fiber.Map{"error": err}})
-	}
-
-	params := &s3.GetObjectInput{
-		Bucket: aws.String(config.AWS_BUCKET()),
-		Key:    aws.String(strconv.Itoa(counter) + "-P24_HGUV6.5.3B01_UPGRADE.bin"),
-	}
-
-	req, _ := s3Client.GetObjectRequest(params)
-	objectURL, err := req.Presign(15 * time.Minute)
-	if err != nil {
-		log.Fatal("[AWS GET LINK]:", params, err)
-	}
+	objectURL, err := utils.S3Get(counter)
 
 	return c.Status(http.StatusCreated).JSON(responses.DefaultResponse{
 		Status:  http.StatusCreated,
